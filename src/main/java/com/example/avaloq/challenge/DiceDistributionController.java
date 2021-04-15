2e042b7f705ab9495536060509ca15ac2af06039
vladimirvlkovic <vladimir.vlkovic@profect.sk>
2021-04-15 14:02:56 +0200

    Makes controller params required

--- src/main/java/com/example/avaloq/challenge/DiceDistributionController.java
+++ src/main/java/com/example/avaloq/challenge/DiceDistributionController.java
@@ -29,9 +29,9 @@ public class DiceDistributionController {
     final DiceDistributionService diceDistributionService;
 
     @GetMapping("api/dice-distribution")
-    public Map<Integer, Integer> getDiceDistributionResult(@RequestParam(required = false, defaultValue = "3") @Min(1) Integer diceNumber,
-                                                           @RequestParam(required = false, defaultValue = "6") @Min(4) Integer sideNumber,
-                                                           @RequestParam(required = false, defaultValue = "100") @Min(1) Integer rollNumber) {
+    public Map<Integer, Integer> getDiceDistributionResult(@RequestParam @Min(1) Integer diceNumber,
+                                                           @RequestParam @Min(4) Integer sideNumber,
+                                                           @RequestParam @Min(1) Integer rollNumber) {
         return diceDistributionService.getDiceDistribution(diceNumber, sideNumber, rollNumber);
     }
     @GetMapping("api/dice-simulation-list")
@@ -40,8 +40,8 @@ public class DiceDistributionController {
     }
 
     @GetMapping("api/relative-dice-distribution")
-    public Map<Integer, BigDecimal> getRelativeDiceDistribution(@RequestParam(required = false, defaultValue = "3") @Min(1) Integer diceNumber,
-                                                                @RequestParam(required = false, defaultValue = "6") @Min(4) Integer sideNumber) {
+    public Map<Integer, BigDecimal> getRelativeDiceDistribution(@RequestParam @Min(1) Integer diceNumber,
+                                                                @RequestParam @Min(4) Integer sideNumber) {
         return diceDistributionService.getRelativeDistribution(diceNumber, sideNumber);
     }
 
